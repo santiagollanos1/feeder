e25d58648f276e96cc62b7b70bc9883d7c45f8bd
santiagollanos1 <santiagodariollanos@gmail.com>
2025-11-14 15:21:17 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,18 +2,34 @@
 #include "serial_command.h"
 #include "settings.h"
 
-DephasedPWM pwm;
+
+volatile uint32_t phase_angle = 0;   // fase global
+DephasedPWM pwm;                     // tu objeto
+
+void TC5_Handler(void) {
+    // limpiar interrupción
+    TC5->COUNT16.INTFLAG.bit.MC0 = 1;
+
+    // llamar a la función de fase
+    pwm.set_phase(phase_angle);
+
+    phase_angle = (phase_angle + 45) % 360;  // desfasar cada interrupción
+    Serial.print(phase_angle);
+}
+
 
 void setup() {
   Serial.begin(115200);
-  pwm.init();
   
+  pwm.init_phaseloop(11500);
+  //pwm.set_phase(360);
+  pwm.init();
 }
 
+
+
+
 void loop() {
-  static uint32_t phase = 0;
-  pwm.set_phase(phase);
-  phase = phase + 10;
-  delay(10);
 }
   
+
