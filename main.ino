2b957f499edbd1b9d5ca4a988c4934416f3cccef
santiagollanos1 <santiagodariollanos@gmail.com>
2025-11-24 05:22:08 -0300

    Updated

--- main.ino
+++ main.ino
@@ -1,35 +1,36 @@
 #include "dephased_pwm.h"
 #include "serial_command.h"
 #include "settings.h"
+#include "vib_control.h"
 
+volatile uint32_t phase_angle = 0;
+volatile int phase_shift = 30;
+DephasedPWM pwm;
 
-volatile uint32_t phase_angle = 0;   // fase global
-DephasedPWM pwm;                     // tu objeto
-
-void TC5_Handler(void) {
-    // limpiar interrupción
-    TC5->COUNT16.INTFLAG.bit.MC0 = 1;
-
-    // llamar a la función de fase
+void TC5_Handler() 
+{
+    TC5->COUNT16.INTFLAG.reg = TC_INTFLAG_MC0; 
     pwm.set_phase(phase_angle);
-
-    phase_angle = (phase_angle + 45) % 360;  // desfasar cada interrupción
-    Serial.print(phase_angle);
+    phase_angle = (phase_angle + phase_shift) % 360;
 }
 
 
 void setup() {
-  Serial.begin(115200);
-  
-  pwm.init_phaseloop(11500);
-  //pwm.set_phase(360);
-  pwm.init();
+    Serial.begin(115200);
+    pinMode(2, OUTPUT);
+    digitalWrite(2, LOW);
+    pinMode(1, OUTPUT);
+    pwm.init();
 }
 
-
-
+void loop() {
+    SerialCommand(pwm);
+    VibrationControl(); 
+}
+  
 
 void loop() {
 }
   
+
 
