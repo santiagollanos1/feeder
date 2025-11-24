4d2f1600492fb988ade31617b151edb64dbb772a
santiagollanos1 <santiagodariollanos@gmail.com>
2025-11-24 05:24:57 -0300

    Update serial_command.h

--- serial_command.h
+++ serial_command.h
@@ -1,6 +1,12 @@
-// serial_command.h
 #ifndef SERIAL_COMMAND_H
 #define SERIAL_COMMAND_H
+
+#include <Arduino.h>
 #include "dephased_pwm.h"
+#include "vib_control.h"
+
+extern volatile int phase_shift;
+
 void SerialCommand(DephasedPWM &pwm);
+
 #endif
